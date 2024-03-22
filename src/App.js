b54f27a7ccf36b46d9ff809810401f2a1bc87b07
Younghyun <yhbae97@gmail.com>
2024-03-22 21:48:20 +0100

    first pairprogramming

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+	const weather_API =
+		"https://api.openweathermap.org/data/2.5/weather?lat=37.55&lon=126.99&appid=0392c2de4574d49fb4740fd4559aa5d2";
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch(weather_API);
+			const data = await response.json();
+			setData(data.results);
+		}
+		fetchData();
+	}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} */}
+      
     </div>
   );
 }
